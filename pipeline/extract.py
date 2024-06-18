c3687fd72669bda4a40fda81da13efea1bee0253
laUmar123 <work.umar7@hotmail.com>
2024-06-18 13:01:09 +0100

    added the functions required to run the extract process

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,15 +1,36 @@
 """This file is responsible for downloading the latest earthquake data"""
-from datetime import datetime
+import datetime
 import requests
 
 URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"
 FEATURES = "features"
+PROPERTIES = "properties"
+TIME = "time"
 
 
-def convert_time_to_current_time(time_in_ms: int) -> datetime:
-    return datetime.timestamp(time_in_ms/1000)
+def get_minute_from_epoch_time(time_in_ms: int) -> datetime:
+    """Given epoch, it converts it to human-readable format"""
+    return datetime.datetime.fromtimestamp(time_in_ms/1000, tz=datetime.timezone.utc).minute
 
 
-response = requests.get(URL)
-data = response.json()[FEATURES]
-print(data)
+def gets_all_earthquake_data() -> list[dict]:
+    """Gets all earthquake data for the hour from USGS"""
+    response = requests.get(URL)
+    data = response.json()[FEATURES][PROPERTIES]
+    return data
+
+
+def get_current_earthquake_data(all_earthquake_data: list[dict]) -> list[dict]:
+    """Gets all the most recent earthquakes from data"""
+    current_minute = datetime.datetime.now().minute
+    print(current_minute)
+    latest_earthquakes = []
+
+    for earthquake in all_earthquake_data:
+        if get_minute_from_epoch_time(earthquake[TIME]) == current_minute:
+            latest_earthquakes.append(earthquake)
+
+
+def extract_process() -> list[dict]:
+    """Runs the functions to extract all relevant data"""
+    ...
