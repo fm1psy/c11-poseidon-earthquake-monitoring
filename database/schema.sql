13080866ad3e2c35b8124e90937997976f0090f4
ejepsen88 <160356300+ejepsen88@users.noreply.github.com>
2024-06-18 14:10:52 +0100

    Rearrange tables in schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -5,34 +5,6 @@ DROP TABLE mag_types CASCADE;
 DROP TABLE networks CASCADE;
 DROP TABLE types CASCADE;
 
-CREATE TABLE earthquakes (
-    earthquake_id VARCHAR UNIQUE NOT NULL,
-    magnitude FLOAT NOT NULL,
-    lon DECIMAL NOT NULL,
-    lat DECIMAL NOT NULL,
-    time TIMESTAMP CHECK (time <= CURRENT_TIMESTAMP),
-    felt SMALLINT,
-    cdi FLOAT,
-    mmi REAL,
-    alert_id SMALLINT NOT NULL,
-    status_id SMALLINT NOT NULL,
-    significance SMALLINT NOT NULL,
-    network_id SMALLINT NOT NULL,
-    nst SMALLINT,
-    dmin REAL,
-    gap REAL,
-    magtype_id SMALLINT NOT NULL,
-    type SMALLINT,
-    title TEXT NOT NULL,
-    depth FLOAT,
-    PRIMARY KEY (earthquake_id),
-    FOREIGN KEY (alert_id) REFERENCES alerts(alert_id),
-    FOREIGN KEY (status_id) REFERENCES status(status_id),
-    FOREIGN KEY (network_id) REFERENCES networks(network_id),
-    FOREIGN KEY (type_id) REFERENCES networks(network_id),
-    FOREIGN KEY (mag_type_id) REFERENCES mag_types(mag_type_id)
-);
-
 CREATE TABLE alerts (
     alert_id UNIQUE SMALLINT GENERATED ALWAYS AS IDENTITY,
     alert_value VARCHAR(6) UNIQUE NOT NULL,
@@ -62,6 +34,35 @@ CREATE TABLE mag_types (
     mag_type_value VARCHAR(3) UNIQUE NOT NULL,
     PRIMARY KEY (mag_type_id)
 );
+
+CREATE TABLE earthquakes (
+    earthquake_id VARCHAR UNIQUE NOT NULL,
+    magnitude FLOAT NOT NULL,
+    lon DECIMAL NOT NULL,
+    lat DECIMAL NOT NULL,
+    time TIMESTAMP CHECK (time <= CURRENT_TIMESTAMP),
+    felt SMALLINT,
+    cdi FLOAT,
+    mmi REAL,
+    alert_id SMALLINT NOT NULL,
+    status_id SMALLINT NOT NULL,
+    significance SMALLINT NOT NULL,
+    network_id SMALLINT NOT NULL,
+    nst SMALLINT,
+    dmin REAL,
+    gap REAL,
+    magtype_id SMALLINT NOT NULL,
+    type_id SMALLINT,
+    title TEXT NOT NULL,
+    depth FLOAT,
+    PRIMARY KEY (earthquake_id),
+    FOREIGN KEY (alert_id) REFERENCES alerts(alert_id),
+    FOREIGN KEY (status_id) REFERENCES status(status_id),
+    FOREIGN KEY (network_id) REFERENCES networks(network_id),
+    FOREIGN KEY (type_id) REFERENCES networks(network_id),
+    FOREIGN KEY (mag_type_id) REFERENCES mag_types(mag_type_id)
+);
+
 
 INSERT INTO alerts VALUES ('green'), ('yellow'), ('orange'), ('red');
 INSERT INTO networks VALUES ('ak'), ('at'), ('ci'), ('hv'), ('ld'), ('mb'), ('nc'), ('nm'), ('nn'), ('pr'), ('pt'), ('se'), ('us'), ('uu'), ('uw');
