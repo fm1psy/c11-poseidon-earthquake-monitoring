bd1e8102330044a1921e5e351f724f8eff4a18e9
joe1606 <josephlamkl75@outlook.com>
2024-06-19 12:00:14 +0100

    begind data validation

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+import logging
+
 example = [{'type': 'Feature',
             'properties': {'mag': 0.67,
                            'place': '13 km WSW of Searles Valley, CA',
@@ -9,7 +12,7 @@ example = [{'type': 'Feature',
                            'felt': None,
                            'cdi': None,
                            'mmi': None,
-                           'alert': None,
+                           'alert': 'error',
                            'status': 'reviewed',
                            'tsunami': 0,
                            'sig': 7,
@@ -29,42 +32,111 @@ example = [{'type': 'Feature',
             'id': 'ci40801680'}]
 
 
-def get_earthquake_property(data, property):
-    return data['properties'][property]
 
 
-def get_earthquake_geometry(data, geometry):
-    return data['geometry'][geometry]
+
+
+PAGER_ALERT_LEVELS = ['green', 'yellow', 'orange', 'red']
+READING_STATUS = ['automatic', 'reviewed', 'deleted']
+NETWORKS = ['ak', 'at', 'ci', 'hv', 'ld', 'mb', 'nc', 'nm', 'nn', 'pr', 'pt', 'se', 'us', 'uu', 'uw']
+MAGNITUDE_TYPES = ['md', 'ml', 'ms', 'mw', 'me', 'mi', 'mb', 'mlg']
+EARTHQUAKE_TYPES = ['earthquake', 'quarry']
+
+def convert_epoch_to_utc(time_in_ms: int) -> datetime:
+    """
+    Given epoch, it converts it to human-readable format
+    """
+
+    full_date = datetime.datetime.fromtimestamp(
+        time_in_ms / 1000, tz=datetime.timezone.utc)
+    
+    return full_date.strftime("%H:%M")
+
+
+def get_earthquake_property(data:dict, property_name: str) -> int | str | None:
+    """
+    Gets earthquake properties from the api data
+    """
+    try:
+        if 'properties' not in data:
+            logging.error('Missing earthquake properties')
+            return None
+        elif property_name not in data['properties']:
+            logging.error('Property "%s" not in data', property_name)
+            return None
+        else:
+            return data['properties'][property_name]
+        
+    except Exception as e:
+        logging.error(f'An unexpected error occurred: {e}')
+        return None
+
+def get_earthquake_geometry(data: dict, geometry_param: str) -> int | None:
+    """
+    Gets earthquake properties from the api data
+    """
+    geometry_index = {'lon': 0, 'lat': 1, 'depth': 2}
+    try:
+        if 'geometry' not in data:
+            logging.error('Missing earthquake geometry')
+            return None
+        if 'coordinates' not in data['geometry']:
+            logging.error('Missing earthquake geometry coordinates')
+            return None
+        if geometry_param not in geometry_index:
+            logging.error('Invalid geometry parameter: "%s"', geometry_param)
+            return None
+        if len(data['geometry']['coordinates']) != 3:
+            logging.error('Earthquake geometry coordinates missing either lon/lat/depth')
+            return None
+        return data['geometry']['coordinates'][geometry_index[geometry_param]]
+    
+    except Exception as e:
+        logging.error(f'An unexpected error occurred: {e}')
+        return None
 
 
 def get_earthquake_id(data):
-    return data['id']
-
+    """
+    Gets earthquake id from api data
+    """
+    try:
+        if 'id' not in data:
+            logging.error('Missing earthquake id')
+            return None
+        return data['id']
+    except Exception as e:
+        logging.error(f'An unexpected error occurred: {e}')
+        return None
 
 def get_earthquake_data(data):
-    status = get_earthquake_property(data, 'status')
-    network = get_earthquake_property(data, 'net')
-    magtype = get_earthquake_property(data,'magType')
-    type = get_earthquake_property(data, 'type')
+    earthquake_id = get_earthquake_id(data)
+    alert = get_earthquake_property(data, 'alert')  #validate_property
+    status = get_earthquake_property(data, 'status')  # validate_property
+    network = get_earthquake_property(data, 'net')  # validate_property
+    magtype = get_earthquake_property(data, 'magType')  # validate_property
+    earthquake_type = get_earthquake_property(data, 'type')  # validate_property
     magnitude = get_earthquake_property(data, 'mag')
-    lon = get_earthquake_geometry(data, 'coordinates')[0]
-    lat = get_earthquake_geometry(data, 'coordinates')[1]
-    depth = get_earthquake_geometry(data, 'coordinates')[2]
+    lon = get_earthquake_geometry(data, 'lon')
+    lat = get_earthquake_geometry(data, 'lat')
+    depth = get_earthquake_geometry(data, 'depth')
     time = get_earthquake_property(data, 'time')
     felt = get_earthquake_property(data, 'felt')
-    cdi = get_earthquake_property(data, 'cdi')
-    mmi = get_earthquake_property(data, 'mmi')
+    cdi = get_earthquake_property(data, 'cdi') #intensity
+    mmi = get_earthquake_property(data, 'mmi') #intensity
     significance = get_earthquake_property(data, 'sig')
-    nst = get_earthquake_property(data,'nst')
-    dmin = get_earthquake_property(data, 'dmin')
-    gap = get_earthquake_property(data, 'gap')
-    title = get_earthquake_property(data, 'title')
+    nst = get_earthquake_property(data,'nst') #number of stations
+    dmin = get_earthquake_property(data, 'dmin') #station distance from epicentre, smaller is better
+    gap = get_earthquake_property(data, 'gap') #gap between 2 stations
+    title = get_earthquake_property(data, 'title') 
 
     return {
+        'earthquake_id': earthquake_id,
+        'alert': validate_property(alert, 'alert'),
         'status': status,
         'network': network,
         'magtype': magtype,
-        'type': type,
+        'earthquake_type': earthquake_type,
         'magnitude': magnitude,
         'longitude': lon,
         'latitude': lat,
@@ -80,10 +152,48 @@ def get_earthquake_data(data):
         'title': title
     }
 
+def validate_property(value, property):
+    """
+    This function can validate earthquake readings for: 
+    alert, status, network, magtype and type.
+    """
+    valid_values = {
+        'alert': PAGER_ALERT_LEVELS,
+        'status': READING_STATUS,
+        'network': NETWORKS,
+        'magtype': MAGNITUDE_TYPES,
+        'type': EARTHQUAKE_TYPES
+    }
 
+    if not isinstance(value, str):
+        logging.error('Invalid data type: expected string')
+        return None
+    if value.lower() not in valid_values[property]:
+        logging.error(f'{property.title()} not recognised. '
+                    f'Value must be: {", ".join(valid_values[property])}')
+        return None
+    
+    return value.lower()
 
+def validate_magnitude(magnitude):
+    if 0.0 <= magnitude <= 10.0:
+        return True
+    else:
+        logging.error(f'Magnitude value {magnitude} out of range. '
+                      f'Value must be between {0.0} and {10.0}.')
+        return False
+
+def clean_data(data):
+    ...
+
+def transform_process():
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    return [get_earthquake_data(data)for data in example]
+       
 
 if __name__ == "__main__":
-    for data in example:
-        print(get_earthquake_data(data))
+    
+    print(transform_process())
+
 
