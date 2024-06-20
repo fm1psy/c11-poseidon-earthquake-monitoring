67e538510e9245e5c8b90d6f95f28e7bd358a683
fm1psy <freddymateo1@gmail.com>
2024-06-20 13:12:47 +0100

    add database connection added database connection functions to the api, and started an endpoint to retrieve all earthquake data.

--- api/api.py
+++ api/api.py
@@ -2,13 +2,60 @@
 Whether it be an amateur developer or a seasoned researcher, this service should be easy to make the most out of through its endpoints."""
 from dotenv import load_dotenv
 from flask import Flask, request
+import psycopg2
+import psycopg2.extras
+from psycopg2.extensions import connection, cursor
+from os import environ as env
 
 app = Flask(__name__)
+
+
+def get_connection() -> connection:
+    """Return a connection object associated with the earthquake database"""
+    return psycopg2.connect(
+        user=env["DB_USERNAME"],
+        password=env["DB_PASSWORD"],
+        host=env["DB_HOST"],
+        port=env["DB_PORT"],
+        database=env["DB_NAME"]
+    )
+
+
+def get_cursor(conn: connection) -> cursor:
+    return conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+
+def get_earthquake_data() -> list[dict]:
+    """return a list of all earthquake data from the database."""
+    conn = get_connection()
+
+    search_query = """SELECT * FROM earthquakes
+    JOIN magtypes ON magtypes.magtype_id= earthquakes.magtype_id
+    JOIN statuses ON statuses.status_id= earthquakes.status_id
+    JOIN types ON types.type_id= earthquakes.type_id
+    JOIN networks ON networks.network_id= earthquakes.network_id
+    JOIN alerts ON alerts.alert_id= earthquakes.alert_id
+    """
+    with get_cursor(conn) as cur:
+        cur.execute(f"{search_query};")
+        fetched_earthquakes = cur.fetchall()
+    conn.close()
+    return fetched_earthquakes
 
 
 @app.route("/", methods=["GET"])
 def endpoint_index():
     return {"message": "Welcome! This is Team Poseidon's API, connected to our Earthquake database. Use this to retrieve information on the earthquake data we have collected."}
+
+
+@app.route("/earthquakes", methods=["GET"])
+def get_earthquakes():
+    if request.method == "GET":
+        try:
+            earthquakes = get_earthquake_data()
+            return earthquakes, 200
+        except Exception as e:
+            return {"error": str(e)}, 400
 
 
 if __name__ == "__main__":
