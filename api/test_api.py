864900fcd6f626ed7a7d7cfa4bcf53f4e36f89d6
fm1psy <freddymateo1@gmail.com>
2024-06-20 16:35:12 +0100

    add testing added tests to validate the endpoint index and the initial earthquake endpoint

--- api/test_api.py
+++ api/test_api.py
@@ -1,2 +1,27 @@
-# pylint: ignore-pylint
-from api import *
+# pylint: skip-file
+from dotenv import load_dotenv
+import pytest
+from api import app
+from unittest.mock import patch
+
+load_dotenv()
+
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_endpoint_index(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "message" in response.json
+
+
+def test_endpoint_get_earthquakes(client):
+    response = client.get("/earthquakes")
+    print(response.json)
+    assert response.status_code == 200
+    assert isinstance(response.json, list)
