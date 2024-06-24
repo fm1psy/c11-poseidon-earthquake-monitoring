b22d65fa5785eea536626bf496cdb02e0fee89d4
joe1606 <josephlamkl75@outlook.com>
2024-06-24 10:06:45 +0100

    pipeline can be run on a single file

---
+++ pipeline/main.py
@@ -0,0 +1,28 @@
+from extract import extract_process
+from transform import transform_process
+from load import load_process
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+if __name__ == "__main__":
+    logging.info('Starting pipeline')
+
+    try:
+        extracted_data = extract_process()
+    except Exception as e:
+        logging.error(f'Error during extraction: {e}')
+    
+    try:
+        transformed_data = transform_process(extracted_data)
+    except Exception as e:
+        logging.error(f'Error during transform: {e}')
+
+    try:
+        load_process(transformed_data)
+        logging.info('Pipeline completed running')
+    except Exception as e:
+        logging.error(f'Error during load: {e}')
+
+
