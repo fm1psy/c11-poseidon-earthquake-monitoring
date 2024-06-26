7ac49bf36f85a37081b0e6a0ffcdce77acc6a864
joe1606 <josephlamkl75@outlook.com>
2024-06-26 10:08:14 +0100

    refractored code, debudding texts

--- pipeline/sns.py
+++ pipeline/sns.py
@@ -6,6 +6,28 @@ import logging
 from psycopg2.extensions import connection, cursor
 import psycopg2.extras
 
+example_data = [{
+    "earthquake_id": "ak0247tc2ogk",
+    "alert": "green",
+    "status": "automatic",
+    "network": "ak",
+    "magtype": "ml",
+    "earthquake_type": "earthquake",
+    "magnitude": 6.0,
+    "lon": -2.964996,
+    "lat": 53.407624,
+    "depth": 0,
+    "time": '2024-06-24 12: 22: 22',
+    "felt": None,
+    "cdi": None,
+    "mmi": None,
+    "significance": 158,
+    "nst": None,
+    "dmin": None,
+    "gap": None,
+    "title": "M 3.2 - 88 km NW of Yakutat, Alaska"
+}]
+
 
 load_dotenv()
 
@@ -37,7 +59,6 @@ def get_connection() -> connection:
 
 def get_cursor(conn: connection) -> cursor:
     """Creates a cursor based on provided psycopg2 connection"""
-    logging.info("Creating a cursor from the psycopg2 connection")
     try:
         return conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     except Exception as e:
@@ -52,15 +73,15 @@ def calculate_distance(topic_coordinates, earthquake_coordinates):
     if within a certain distance based of dmin/mag then will
     send notifications to all users subscribed to that topic
     """
-    return haversine(topic_coordinates, earthquake_coordinates)
-
+    return int(haversine(topic_coordinates, earthquake_coordinates))
 
 
 def get_topics(conn):
     """
     Gets topics from database
     """
-    with conn.cursor() as cur:
+    cursor = get_cursor(conn)
+    with cursor as cur:
         query = """SELECT * FROM topics;"""
         cur.execute(query)
         topic_coordinates = cur.fetchall()
@@ -84,6 +105,15 @@ def check_topics_in_range(eq_lon, eq_lat, topic):
     return False
 
 
+def get_user_information(user):
+    return {
+        'user_id': user['user_id'],
+        'email_address': user['email_address'],
+        'phone_number': user['phone_number'],
+        'topic_arn': user['topic_arn'],
+        'min_magnitude': user['min_magnitude']
+    }
+
 
 def get_subscribed_users(conn, related_topics):
     """
@@ -91,11 +121,11 @@ def get_subscribed_users(conn, related_topics):
     topics.
     """
     interested_users = []
-
-    with conn.cursor() as cur:
+    cursor = get_cursor(conn)
+    with cursor as cur:
         for topic in related_topics:
             query = """
-            SELECT uta.user_id, u.email_address, u.phone_number, u.sms_subscription_arn, u.email_subscription_arn, t.min_magnitude
+            SELECT uta.user_id, u.email_address, u.phone_number, t.topic_arn, t.min_magnitude
             FROM user_topic_assignments AS uta
             JOIN users AS u ON u.user_id = uta.user_id
             JOIN topics AS t ON uta.topic_id = t.topic_id
@@ -106,45 +136,57 @@ def get_subscribed_users(conn, related_topics):
 
             for row in rows:
                 user = {
-                    'user_id': row[0],
-                    'email_address': row[1],
-                    'phone_number': row[2],
-                    'sms_subscription_arn': row[3],
-                    'email_subscription_arn': row[4],
-                    'min_magnitude': row[5]
+                    'user_id': row['user_id'],
+                    'email_address': row['email_address'],
+                    'phone_number': row['phone_number'],
+                    'topic_arn': row['topic_arn'],
+                    'min_magnitude': row['min_magnitude']
                 }
-                if user not in interested_users:  # Avoid duplicates
+                print(user['phone_number'])
+                if user not in interested_users:
                     interested_users.append(user)
 
     return interested_users
 
 
-def find_related_topics(transform_data, topics):
+def find_related_topics(latest_earthquakes, topics):
     related_topics = []
-    for data in transform_data:
+    for earthquakes in latest_earthquakes:
         for topic in topics:
-            if data['mag'] >= topics['min_magnitude'] and check_topics_in_range(topic, eq_lon=data['lon'], eq_lat=data['lat']):
+            if earthquakes['magnitude'] >= topic['min_magnitude'] and check_topics_in_range(earthquakes['lon'], earthquakes['lat'], topic):
                 related_topics.append(topic)
     return related_topics
 
 
-def publish_text(sns_client, phone_number, message):
-    """Publishes a text message via SNS"""
-    sns_client.publish(
-        PhoneNumber=phone_number,
-        Message=message
-    )
-
-
-
-def publish_email(sns_client, email_arn, subject, message):
+def send_message(sns_client, arn, subject, message):
     """Publishes an email message via SNS"""
     sns_client.publish(
-        TopicArn=email_arn,
+        TargetArn=arn,
         Message=message,
         Subject=subject
     )
 
+def send_text(sns_client, phone_number, message):
+    sns_client.publish(
+            PhoneNumber=phone_number,
+            Message=message
+        )
+
+def sns_alert_system(earthquakes):
+    sns_client = get_sns_client()
+    conn = get_connection()
+
+    topics = get_topics(conn)
+    related_topics = find_related_topics(earthquakes, topics)
+    subscribed_users = get_subscribed_users(conn, related_topics)
+
+    for user in subscribed_users:
+        message = f"Earthquake Alert! Magnitude {user['min_magnitude']} or above earthquake detected in your area"
+        subject = "Earthquake Alert"
+        send_text(sns_client, user['phone_number'], message)
+        # send_message(
+        #     sns_client, user['topic_arn'], subject, message)
+        
 
 if __name__ == "__main__":
     import extract
@@ -153,43 +195,4 @@ if __name__ == "__main__":
     extract_data = extract.extract_process()
     transform_data = transform.transform_process(extract_data)
 
-    sns_client = get_sns_client()
-    conn = get_connection()
-
-
-    topics = get_topics(conn)
-    related_topics = find_related_topics(transform_data, topics)
-    subscribed_users = get_subscribed_users(conn, related_topics)
-
-    for user in subscribed_users:
-        message = f"Earthquake Alert! Magnitude {user['min_magnitude']} or above earthquake detected in your area"
-        publish_text(sns_client, user['phone_number'], message)
-        subject = "Earthquake Alert"
-        publish_email(
-            sns_client, user['email_subscription_arn'], subject, message)
-
-    conn.close()
-
-
-
-
-# def publish_text(sns_client):
-#     ...
-
-
-# def publish_email(sns_client):
-#     ...
-
-
-
-# if __name__ == "__main__":
-#     import extract
-#     import transform
-#     extract_data = extract.extract_process()
-#     transform_data = transform.transform_process(extract_data)
-
-#     client = get_sns_client()
-#     conn = get_connection()
-#     topics = get_topics(conn)
-#     related_topics = find_related_topics(transform_data, topics)
-#     subscribed_users = get_subscribed_users(conn, related_topics)
+    sns_alert_system(example_data)
--- pipeline/test_sns.py
+++ pipeline/test_sns.py
@@ -2,7 +2,7 @@ from sns import *
 
 from unittest.mock import patch, MagicMock
 import boto3
-
+import pytest
 
 def mock_env_get(key):
     if key == "ACCESS_KEY":
@@ -10,15 +10,15 @@ def mock_env_get(key):
     elif key == "SECRET_ACCESS_KEY":
         return 'fake_secret_key'
     elif key == "DB_HOST":
-        return 'fake_db_host'
+        return 'fake_host'
     elif key == "DB_NAME":
-        return 'fake_db_name'
+        return 'fake_name'
     elif key == "DB_USERNAME":
-        return 'fake_db_username'
+        return 'fake_username'
     elif key == "DB_PASSWORD":
-        return 'fake_db_password'
+        return 'fake_password'
     elif key == "DB_PORT":
-        return 'fake_db_port'
+        return 'fake_port'
 
 
 @patch('sns.boto3.client')
@@ -32,8 +32,8 @@ def test_get_sns_client_success(mock_env_get, mock_boto_client):
     mock_env_get.assert_any_call("ACCESS_KEY")
     mock_env_get.assert_any_call("SECRET_ACCESS_KEY")
     mock_boto_client.assert_called_once_with('sns',
-                                             aws_access_key_id='dummy_access_key',
-                                             aws_secret_access_key='dummy_secret_key')
+                                             aws_access_key_id='fake_access_key',
+                                             aws_secret_access_key='fake_secret_key')
     assert client == mock_client
 
 
@@ -45,5 +45,91 @@ def test_get_connection(mock_env_get, mock_connection):
 
     conn = get_connection()
 
-    mock_env_get.assert_any_call("ACCESS_KEY")
-    mock_env_get.assert_any_call("SECRET_ACCESS_KEY")
+    mock_env_get.assert_any_call("DB_HOST")
+    mock_env_get.assert_any_call("DB_NAME")
+    mock_env_get.assert_any_call("DB_USERNAME")
+    mock_env_get.assert_any_call("DB_PASSWORD")
+    mock_env_get.assert_any_call("DB_PORT")
+    mock_connection.assert_called_once_with(host='fake_host',
+                                            database='fake_name',
+                                            user='fake_username',
+                                            password='fake_password',
+                                            port='fake_port')
+
+
+    assert conn == mock_conn
+
+
+@patch('sns.psycopg2.connect')
+def test_get_cursor(mock_connect):
+    mock_conn = MagicMock(spec=connection)
+    mock_cursor = MagicMock(spec=psycopg2.extensions.cursor)
+    mock_cursor_factory = MagicMock(spec=psycopg2.extras.RealDictCursor)
+    mock_conn.cursor.return_value = mock_cursor
+
+
+    psycopg2.extras.RealDictCursor = mock_cursor_factory
+
+    cursor = get_cursor(mock_conn)
+
+    mock_conn.cursor.assert_called_once_with(
+        cursor_factory=psycopg2.extras.RealDictCursor)
+    assert cursor == mock_cursor
+
+
+@pytest.mark.parametrize("location_1, location_2, expected_value", 
+    ([
+        ((40.7128, -74.0060), (51.5074, -0.1278), 5570), 
+        ((48.8566, 2.3522), (55.7558, 37.6176), 2486), 
+        ((34.0522, -118.2437), (19.4326, -99.1332), 2490), 
+        ((35.6895, 139.6917), (37.7749, -122.4194), 8270), 
+        ((55.7558, 37.6176), (55.6761, 12.5683), 1560), 
+        ((51.5074, -0.1278), (41.9028, 12.4964), 1433),
+        ((19.4326, -99.1332), (-33.4489, -70.6693), 6610),
+        ((37.7749, -122.4194), (-33.8688, 151.2093), 11947),
+        ((40.7128, -74.0060), (39.9042, 116.4074), 10989),
+        ((35.6895, 139.6917), (22.3193, 114.1694), 2879)
+    ]))
+def test_calculate_distance(location_1, location_2, expected_value):
+    assert calculate_distance(location_1, location_2) == expected_value
+
+
+def test_get_topics_success():
+
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+    mock_conn.cursor.return_value.__enter__.return_value = mock_cursor
+
+    mock_topic_data = [('Topic 1', 'Description 1'),
+                       ('Topic 2', 'Description 2')]
+    mock_cursor.fetchall.return_value = mock_topic_data
+
+
+    topic_coordinates = get_topics(mock_conn)
+
+    mock_conn.cursor.assert_called_once()
+    mock_cursor.execute.assert_called_once_with("SELECT * FROM topics;")
+    assert topic_coordinates == mock_topic_data
+
+
+@pytest.mark.parametrize("topic, detail, expected_result", [
+    ({'topic_arn': 'arn:aws:sns:us-east-1:123456789012:MyTopic'},
+     'topic_arn', 'arn:aws:sns:us-east-1:123456789012:MyTopic'),
+    ({'lon': '-74.0060'}, 'lon','-74.0060'),
+    ({'lat': '40.7128'}, 'lat', '40.7128'),
+    ({'min_magnitude': '5.0'}, 'min_magnitude', '5.0')
+])
+def test_get_topic_detail(topic, detail, expected_result):
+    assert get_topic_detail(topic, detail) == expected_result
+
+
+def test_check_topic_in_range(liverpool_earthquake, example_topic):
+    lon = liverpool_earthquake[0]['lon']
+    lat = liverpool_earthquake[0]['lat']
+    assert check_topics_in_range(lon, lat, example_topic) == True
+
+
+def test_check_topic_not_in_range(example_transformed_data, example_topic):
+    lon = example_transformed_data[0]['lon']
+    lat = example_transformed_data[0]['lat']
+    assert check_topics_in_range(lon, lat, example_topic) == False
