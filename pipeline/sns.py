8f25f8f9968b1df032625524fb90df6b6738a651
joe1606 <josephlamkl75@outlook.com>
2024-06-27 11:48:15 +0100

    fixed pylint

--- pipeline/sns.py
+++ pipeline/sns.py
@@ -250,11 +250,11 @@ SAFETY TIPS:
 
 Stay safe and follow these guidelines until the shaking subsides. Be aware of potential aftershocks.
 """
-            )
-            subject = "EARTHQUAKE WARNING"
-            send_message(
-                sns_client, user['topic_arn'], subject, message
-            )
+                )
+                subject = "EARTHQUAKE WARNING"
+                send_message(
+                    sns_client, user['topic_arn'], subject, message
+                )
 
 
 if __name__ == "__main__":
@@ -262,28 +262,5 @@ if __name__ == "__main__":
     import transform
     extract_data = extract.extract_process()
     transform_data = transform.transform_process(extract_data)
-    example_data = [{
-        "earthquake_id": "ak0247tc2ogk",
-        "alert": "green",
-        "status": "automatic",
-        "network": "ak",
-        "magtype": "ml",
-        "earthquake_type": "earthquake",
-        "magnitude": 6.0,
-        "lon": -2.964996,
-        "lat": 53.607624,
-        "depth": 0,
-        "time": '2024-06-24 12: 22: 22',
-        "felt": None,
-        "cdi": None,
-        "mmi": None,
-        "significance": 158,
-        "nst": None,
-        "dmin": None,
-        "gap": None,
-        "title": "M 3.2 - 88 km NW of Yakutat, Alaska"
-    }
-    ]
     if transform_data:
         sns_alert_system(transform_data)
-    sns_alert_system(example_data)
