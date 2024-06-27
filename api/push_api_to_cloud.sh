4749919da27d170a95c22875004f8c5e7e58de6c
fm1psy <freddymateo1@gmail.com>
2024-06-27 10:16:25 +0100

    add variables to bash script

--- api/push_api_to_cloud.sh
+++ api/push_api_to_cloud.sh
@@ -1,4 +1,6 @@
-aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 129033205317.dkr.ecr.eu-west-2.amazonaws.com
-docker build --platform linux/amd64 -t c11-poseidon-api .
-docker tag c11-poseidon-api:latest 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c11-poseidon-api:latest
-docker push 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c11-poseidon-api:latest
+ecr_arn=129033205317.dkr.ecr.eu-west-2.amazonaws.com
+ecr_name=c11-poseidon-api
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin $ecr_arn
+docker build --platform linux/amd64 -t $ecr_name .
+docker tag $ecr_name:latest $ecr_arn/$ecr_name:latest
+docker push $ecr_arn/$ecr_name:latest
